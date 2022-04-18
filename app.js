347e78984183b8f15662e89152d7c07dcc443a6f
odonald <eugene.odonald@code.berlin>
2022-04-18 18:28:01 +0200

    Took one day to make the slide in animation work. Got hints from my post on stackoverflow. Figured that I had to add a constant per class in my IntersectionObserver.

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Add the animation class
-        entry.target.classList.add('rectangle-animation-one');
-
-      }
-    });
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('rectangle-animation-one', entry.isIntersecting);
+    }
   });
-  
-  observer.observe(document.querySelector('.rectangle-one'));
-  
- 
-  
+});
+
+observer.observe(document.querySelector('.rectangle-one'));
+
+const observer2 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('rectangle-animation-two', entry.isIntersecting);
+    }
+  });
+});
+
+observer2.observe(document.querySelector('.rectangle-two'));
+
+const observer3 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('rectangle-animation-three', entry.isIntersecting);
+    }
+  });
+});
+observer3.observe(document.querySelector('.rectangle-three'));
+
+
