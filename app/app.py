8f05a48952bfee73f1e30c6f7b9833039cb2368f
odonald <eugene.odonald@code.berlin>
2022-04-22 16:32:47 +0200

    Big day. Got the website to run via FLASK. Built a template. Built router for all pages. Changed all links to work with new routes. created static folder for img/css/js

---
+++ app/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/comments")
+def comments():
+    return render_template("comment-page.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/footer")
+def footer():
+    return render_template("footer.html")
+
+@app.route("/impressum")
+def impressum():
+    return render_template("impressum.html")
+
+@app.route("/legal")
+def legal():
+    return render_template("legal.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/navbar")
+def navbar():
+    return render_template("navbar.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -1,4 +1,4 @@
-fetch('navbar.html')
+fetch('/navbar')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
