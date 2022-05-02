591d810a95b57388c87ce0d67f75a0c6bcd492f1
odonald <eugene.odonald@code.berlin>
2022-05-02 02:30:47 +0200

    bigfixing for heroku deployment - Changed sqlalchemy db url

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from os import environ
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = environ.get('SECRET_KEY')
 
